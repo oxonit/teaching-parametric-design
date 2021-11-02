61ea0914123a4e0d746f9bb2b0f72c12804c0eef
oxonit <maximilian.olstedt@gmail.com>
2021-11-02 15:52:15 +0100

    Hausaufgabe 1

---
+++ code/task-01/sketch.js
@@ -0,0 +1,13 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(0, 121, 80);
+  fill(249, 228, 183);
+rect(0, 0, 50, 400);
+rect(0, 0, 200, 133,3)
+rect(0, 266, 200, 133,3)
+rect(200, 133, 150, 133,3)
+}
+
